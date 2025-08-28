0ba468b06e2230802f8632439922dfc2ee2cdb2e
jeongbanseog <wibalislfilask@gmail.com>
2025-08-28 16:57:51 +0900

    프로젝트 생성

---
+++ src/main/java/com/example/share_note/ShareNoteApplication.java
@@ -0,0 +1,13 @@
+package com.example.share_note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/share_note/ShareNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.share_note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
